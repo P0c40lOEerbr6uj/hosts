3ac3fbe7a356da250e0ee5e8db9ed873f9dccd6a
P0c40lOEerbr6uj <pi@raspberrypi>
2020-09-30 23:38:55 +0200

    new CA

--- 00003-add-ca-certificate.sh
+++ 00003-add-ca-certificate.sh
@@ -1,4 +1,4 @@
-CA="https://raw.githubusercontent.com/P0c40lOEerbr6uj/hosts/master/b34f1749.0"
+CA="https://raw.githubusercontent.com/P0c40lOEerbr6uj/hosts/master/94346cbb.0"
 echo "Downloading Root CA certificate $CA"
 echo "Build dir: $BUILD_DIR"
 echo "Build dir files: $(ls $BUILD_DIR)"
